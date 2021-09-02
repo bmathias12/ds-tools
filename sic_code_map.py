5d37ec0f60d11c13e55983718d28a804719e4434
bmathias12 <bmathias@speedeondata.com>
2021-09-02 11:30:51 -0400

    add sic_code_map.py

---
+++ sic_code_map.py
@@ -0,0 +1,104 @@
+sic_codes_2digit = {
+    '01': 'Agricultural Production - Crops',
+    '02': 'Agricultural Production - Livestock',
+    '07': 'Agricultural Services',
+    '08': 'Forestry',
+    '09': 'Fishing, Hunting and Trapping',
+    '10': 'Metal Mining',
+    '12': 'Bituminous Coal and Lignite Mining',
+    '13': 'Oil and Gas Extraction',
+    '14': 'Mining and Quarrying of Nonmetallic Minerals, except Fuels',
+    '15': 'Building Construction General Contractors and Operative Builders',
+    '16': 'Heavy Construction other than Building Construction Contractors',
+    '17': 'Construction Special Trade Contractors',
+    '20': 'Food and Kindred Products',
+    '21': 'Tobacco Products',
+    '22': 'Textile Mill Products',
+    '23': 'Apparel and other Finished Products Made from Fabrics',
+    '24': 'Lumber and Wood Products, except Furniture',
+    '25': 'Furniture and Fixtures',
+    '26': 'Paper and Allied Products',
+    '27': 'Printing, Publishing, and Allied Industries',
+    '28': 'Chemicals and Allied Products',
+    '29': 'Petroleum Refining and Related Industries',
+    '30': 'Rubber and Miscellaneous Plastics Products',
+    '31': 'Leather and Leather Products',
+    '32': 'Stone, Clay, Glass, and Concrete Products',
+    '33': 'Primary Metal Industries',
+    '34': 'Fabricated Metal Products, except Machinery and Transportation Equipment',
+    '35': 'Industrial and Commercial Machinery and Computer Equipment',
+    '36': 'Electronic and other Electrical Equipment and Components, except Computer Equipment',
+    '37': 'Transportation Equipment',
+    '38': 'Measuring, Analyzing, and Controlling Instruments; Photographic, Medical and Optical Goods; Watches and Clocks',
+    '39': 'Miscellaneous Manufacturing Industries',
+    '40': 'Railroad Transportation',
+    '41': 'Local and Suburban Transit and Interurban Highway Passenger Transportation',
+    '42': 'Motor Freight Transportation and Warehousing',
+    '43': 'United States Postal Service',
+    '44': 'Water Transportation',
+    '45': 'Transportation by Air',
+    '46': 'Pipelines, except Natural Gas',
+    '47': 'Transportation Services',
+    '48': 'Communications',
+    '49': 'Electric, Gas and Sanitary Services',
+    '50': 'Wholesale Trade - Durable Goods',
+    '51': 'Wholesale Trade - Nondurable Goods',
+    '52': 'Building Materials, Hardware, Garden Supply, and Mobile Home Dealers',
+    '53': 'General Merchandisse Stores',
+    '54': 'Food Stores',
+    '55': 'Automotive Dealers and Gasoline Service Stations',
+    '56': 'Apparel and Accessory Stores',
+    '57': 'Home Furniture, Furnishings, and Equipment Stores',
+    '58': 'Eating and Drinking Places',
+    '59': 'Miscellaneous Retail',
+    '60': 'Depository Institutions',
+    '61': 'Non-Depository Credit Institutions',
+    '62': 'Security and Commodity Brokers, Dealers, Exchanges, and Services',
+    '63': 'Insurance Carriers',
+    '64': 'Insurance Agents, Brokers and Service',
+    '65': 'Real Estate',
+    '66': 'Holding and other Investment Offices',
+    '70': 'Hotels, Rooming Houses, Camps, and other Lodging Places',
+    '72': 'Personal Services',
+    '73': 'Business Services',
+    '74': 'Automotive Repair, Services, and Parking',
+    '75': 'Automotive Repair, Services, and Parking',
+    '76': 'Miscellaneous Repair Services',
+    '78': 'Motion Pictures',
+    '79': 'Amusement and Recreation Services',
+    '80': 'Health Services',
+    '81': 'Legal Services',
+    '82': 'Educational Services',
+    '83': 'Social Services',
+    '84': 'Museums, Art Galleries, and Botanical and Zoological Gardens',
+    '86': 'Membership Organizations',
+    '87': 'Engineering, Accounting, Research, Management, and Related Services',
+    '88': 'Private Households',
+    '89': 'Miscellaneous Services',
+    '91': 'Executive, Legislative, and General Government, except Finance',
+    '92': 'Justice, Public Order, and Safety',
+    '93': 'Public Finance, Taxation, and Monetary Policy',
+    '94': 'Administration of Human Resource Programs',
+    '95': 'Administration of Environmental Quality and Housing Programs',
+    '96': 'Administration of Economic Programs',
+    '97': 'National Security and International Affairs',
+    '99': 'Nonclassifiable Establishments'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
