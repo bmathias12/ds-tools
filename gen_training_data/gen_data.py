5b60d7367566243b377b14bb6bc67d4e877972a0
bmathias12 <bmathias@speedeondata.com>
2023-10-11 15:51:28 -0400

    split out into module

--- gen_training_data/gen_data.py
+++ gen_training_data/gen_data.py
@@ -2,16 +2,14 @@ import argparse
 import os
 import tempfile
 
+from src.make_classification import MakeClassification
 
-import pandas as pd
-from sklearn.datasets import make_classification
 
 def main(output_dir, output_name, n_sets, n_samples, n_features, seed,):
     
     make_classification = MakeClassification(
         output_dir=output_dir,
         output_name=output_name,
-        n_sets=n_sets,
         seed=seed,
     )
 
@@ -24,31 +22,10 @@ def main(output_dir, output_name, n_sets, n_samples, n_features, seed,):
         n_classes=2,
     )
 
+    make_classification.split(n_sets=n_sets)
+
     out_loc = os.path.join(output_dir, output_name)
     print(out_loc)
-
-class MakeClassification:
-    def __init__(self, output_dir, output_name, n_sets, seed):
-        self.output_dir = output_dir
-        self.output_name = output_name
-        self.n_sets = n_sets
-        self.seed = seed
-
-    def generate(self, n_samples, n_features, n_informative, n_redundant, n_repeated, n_classes):
-
-        X, y = make_classification(
-            n_samples=n_samples,
-            n_features=n_features,
-            n_informative=n_informative,
-            n_redundant=n_redundant,
-            n_repeated=n_repeated,
-            n_classes=n_classes,
-            random_state=self.seed,
-        )
-        df_X = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(X.shape[1])])
-        y = pd.Series(y, name='target')
-
-        self.df = pd.concat([df_X, y], axis=1)
 
 
 if __name__ == '__main__':
@@ -56,7 +33,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-o', '--output_dir', type=str, default=tempfile.gettempdir(), help='Output directory')
     parser.add_argument('--output_name', type=str, default='gen_ml_data', help='Name of the dataset')
-    parser.add_argument('--n_sets', type=int, choices=[1, 2, 3], help='Choose 1, 2, or 3 for the number of sets to generate')
+    parser.add_argument('--n_sets', type=int, choices=[1, 2, 3], default=1, help='Choose 1, 2, or 3 for the number of sets to generate')
     parser.add_argument('-n', '--n_samples', type=int, default=1000, help='Number of samples to generate')
     parser.add_argument('-f', '--n_features', type=int, default=20, help='Number of features to generate')
     parser.add_argument('-s', '--seed', type=int, default=None, help='Random seed')
---
+++ gen_training_data/src/make_classification.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.datasets import make_classification
+
+
+class MakeClassification:
+    def __init__(self, output_dir, output_name, seed):
+        self.output_dir = output_dir
+        self.output_name = output_name
+        self.seed = seed
+
+    def generate(self, n_samples, n_features, n_informative, n_redundant, n_repeated, n_classes):
+        """Generate a classification dataset
+        
+        Args:
+            n_samples (int): Number of samples to generate
+            n_features (int): Number of features to generate
+            n_informative (int): Number of informative features
+            n_redundant (int): Number of redundant features
+            n_repeated (int): Number of duplicated features
+            n_classes (int): Number of classes
+
+        Returns:
+            None
+        """
+        X, y = make_classification(
+            n_samples=n_samples,
+            n_features=n_features,
+            n_informative=n_informative,
+            n_redundant=n_redundant,
+            n_repeated=n_repeated,
+            n_classes=n_classes,
+            random_state=self.seed,
+        )
+        df_X = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(X.shape[1])])
+        y = pd.Series(y, name='target')
+
+        self.df = pd.concat([df_X, y], axis=1)
+
+    def split(self, n_sets):
+        """Split the dataset into train, validation, and test sets
+        
+        Returns:
+            None
+        """
+        if not hasattr(self, 'df'):
+            raise ValueError('Dataset has not been generated yet. Run `generate` method first.')
+        
+        if n_sets == 1:
+            self.df_train = self.df
+            self.df_val = None
+            self.df_test = None
+        elif n_sets == 2:
+            self.df_train = self.df.sample(frac=0.8, random_state=self.seed)
+            self.df_val = self.df.drop(self.df_train.index)
+            self.df_test = None
+        elif n_sets == 3:
+            self.df_train = self.df.sample(frac=0.8, random_state=self.seed)
+            self.df_val = self.df.drop(self.df_train.index).sample(frac=0.5, random_state=self.seed)
+            self.df_test = self.df.drop(self.df_train.index).drop(self.df_val.index)
+        else:
+            raise ValueError('n_sets must be 1, 2, or 3.')
