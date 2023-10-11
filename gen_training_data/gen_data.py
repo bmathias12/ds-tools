be64eb5336c56b29cc4b9061dc43d0279c040d1a
bmathias12 <bmathias@speedeondata.com>
2023-10-11 14:11:35 -0400

    add output dir and n_sets

--- gen_training_data/gen_data.py
+++ gen_training_data/gen_data.py
@@ -1,10 +1,12 @@
 import argparse
+import os
 import tempfile
+
 
 import pandas as pd
 from sklearn.datasets import make_classification
 
-def main(output_dir, num_samples, num_features, seed):
+def main(output_dir, output_name, n_sets, num_samples, num_features, seed,):
     
     X, y = make_classification(
         n_samples=num_samples,
@@ -19,14 +21,19 @@ def main(output_dir, num_samples, num_features, seed):
     df_X = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(X.shape[1])])
     y = pd.Series(y, name='target')
 
-    print(output_dir)
+    out_loc = os.path.join(output_dir, output_name)
+    print(out_loc)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate training data')
+
+    parser.add_argument('-o', '--output_dir', type=str, default=tempfile.gettempdir(), help='Output directory')
+    parser.add_argument('--output_name', type=str, default='gen_ml_data', help='Name of the dataset')
+    parser.add_argument('--n_sets', type=int, choices=[1, 2, 3], help='Choose 1, 2, or 3 for the number of sets to generate')
     parser.add_argument('-n', '--num_samples', type=int, default=1000, help='Number of samples to generate')
     parser.add_argument('-f', '--num_features', type=int, default=20, help='Number of features to generate')
-    parser.add_argument('-o', '--output_dir', type=str, default=tempfile.gettempdir(), help='Output directory')
     parser.add_argument('-s', '--seed', type=int, default=None, help='Random seed')
+    
     args = parser.parse_args()
 
-    main(args.output_dir, args.num_samples, args.num_features, args.seed)
+    main(args.output_dir, args.output_name, args.n_sets, args.num_samples, args.num_features, args.seed)
