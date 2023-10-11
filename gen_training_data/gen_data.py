3255958da5f3ba4f59b2a5e174cc2c7246e6b1f4
bmathias12 <bmathias@speedeondata.com>
2023-10-11 13:44:59 -0400

    fix arg name

--- gen_training_data/gen_data.py
+++ gen_training_data/gen_data.py
@@ -9,7 +9,7 @@ def main(num_samples):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate training data')
-    parser.add_argument('--n_samples', type=int, default=1000, help='Number of samples to generate')
+    parser.add_argument('-n, --num_samples', type=int, default=1000, help='Number of samples to generate')
     args = parser.parse_args()
 
     main(args.num_samples)
