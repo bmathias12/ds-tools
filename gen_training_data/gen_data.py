13b4d55137872e657a6e200e43e9df9a1e023de4
bmathias12 <bmathias@speedeondata.com>
2023-10-11 13:43:23 -0400

    add template for generating data

---
+++ gen_training_data/gen_data.py
@@ -0,0 +1,15 @@
+import argparse
+import tempfile
+
+import pandas as pd
+from sklearn.datasets import make_classification
+
+def main(num_samples):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate training data')
+    parser.add_argument('--n_samples', type=int, default=1000, help='Number of samples to generate')
+    args = parser.parse_args()
+
+    main(args.num_samples)
